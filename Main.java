749fe3e67a721095c7f5accb7b1f17449eb8ece4
heye3775 <heye3775@gmail.com>
2025-07-17 04:34:21 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("Name: John Doe");
+      System.out.println("Age: 20");
+      System.out.println("Course: Object Oriented Programming");
+  }
+}
